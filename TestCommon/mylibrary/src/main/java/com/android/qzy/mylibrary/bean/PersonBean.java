75391215fcf1e311496addc2a801c6753cfe91ed
qzy <43081438@qq.com>
2017-05-12 16:38:20 +0800

    add

---
+++ TestCommon/mylibrary/src/main/java/com/android/qzy/mylibrary/bean/PersonBean.java
@@ -0,0 +1,30 @@
+package com.android.qzy.mylibrary.bean;
+
+/**
+ * 作者：quzongyang
+ * <p>
+ * 创建时间：2017/5/12
+ * <p>
+ * 类描述：
+ */
+
+public class PersonBean {
+    public String name;
+    public int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
